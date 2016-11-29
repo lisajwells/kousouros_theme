ad1530dc354b2e3df43c490c88b40b5c90ff0007
Lisa Wells <lisawells999@gmail.com>
2016-11-29 12:35:18 -0500

    http to https content-home-press.php

--- content-home-press.php
+++ content-home-press.php
@@ -6,18 +6,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-5 col-md-offset-1">
-                <h2><?php the_field('home_press_title'); ?></h2>    
-                <p><?php the_field('home_press_subhed'); ?><p>
+                <h2><?php the_field('home_press_title'); ?></h2>
+                <p><?php the_field('home_press_subhed'); ?></p>
                 <p id="home_press_btn_lrg" style="text-align: center;"><a class="btn btn-hollow-blackborder" href="<?php bloginfo('url'); ?>/press-media">See more press/media</a></p>
             </div>
 
             <div class="col-md-5">
                 <figure class="video" id="home-press-1-group">
                     <?php $video_id = get_field( 'home_press_video_id_1' ); ?>
-                    <a href="#" data-toggle="modal" data-theVideo="http://www.youtube.com/embed/<?php echo $video_id?>">
-                    <img class="youtube" rel="<?php echo $video_id?>" src="http://img.youtube.com/vi/<?php echo $video_id?>/mqdefault.jpg" /></a>
+                    <a href="#" data-toggle="modal" data-theVideo="https://www.youtube.com/embed/<?php echo $video_id?>">
+                    <img class="youtube" alt="video of james kousouros" rel="<?php echo $video_id?>" src="https://img.youtube.com/vi/<?php echo $video_id?>/mqdefault.jpg" /></a>
                 </figure>
-                
+
                 <p id="home_press_btn_sm" style="text-align: center;"><a class="btn btn-hollow-blackborder" href="<?php bloginfo('url'); ?>/press-media">See more press/media</a></p>
             </div>
 
