ea90f65b8f0ed99afa1d842612ca5c310ca308b6
lisajwells <lisa@lisajaynewells.com>
2018-04-18 14:40:33 -0400

    add unnecessary search redirect to for notables intro

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Theme: Flat Bootstrap
- * 
+ *
  * Functions that relate to any tags or functions used in the WordPress templates.
  *
  * Functions for comments, multi-category blog, excerpts, etc.
@@ -19,26 +19,28 @@ add_filter( 'get_the_excerpt', 'xsbf_get_the_excerpt' );
 function xsbf_get_the_excerpt( $excerpt ) {
 
 
-	if ( in_category( 'testimonials' )) { 
-		$page_to_go_to = 'Testimonials'; 
+	if ( in_category( 'testimonials' )) {
+		$page_to_go_to = 'Testimonials';
 	} elseif ( in_category ( 'profile-associate' )) {
-		$page_to_go_to = 'Associates';  
+		$page_to_go_to = 'Associates';
+	} elseif ( in_category ( 'notable-cases' )) {
+		$page_to_go_to = 'Introduction'; // introduction page
 	} elseif ( in_category ( 'white-collar-general' )) {
-		$page_to_go_to = 'White Collar: General'; // notables: white-collar-general 
+		$page_to_go_to = 'White Collar: General'; // notables: white-collar-general
 	} elseif ( in_category ( 'healthcare-and-pharmaceutical' )) {
-		$page_to_go_to = 'White Collar: Healthcare/Pharmaceutical'; // notables: white-collar-healthcare-pharmaceutical 
+		$page_to_go_to = 'White Collar: Healthcare/Pharmaceutical'; // notables: white-collar-healthcare-pharmaceutical
 	} elseif ( in_category ( 'homicide' )) {
-		$page_to_go_to = 'Homicide'; // notables: homicide 
+		$page_to_go_to = 'Homicide'; // notables: homicide
 	} elseif ( in_category ( array( 'racketeering', 'bribery' ))) {
-		$page_to_go_to = 'Racketeering/Bribery'; // notables: racketeering-bribery 
+		$page_to_go_to = 'Racketeering/Bribery'; // notables: racketeering-bribery
 	} elseif ( in_category ( 'narcotics' )) {
-		$page_to_go_to = 'Narcotics'; // notables: narcotics 
+		$page_to_go_to = 'Narcotics'; // notables: narcotics
 	} elseif ( in_category ( array( 'miscellaneous-notables', 'appeals', 'extradition' ))) {
-		$page_to_go_to = 'Miscellaneous/Appeals/Extradition'; // notables: miscellaneous-appeals-extradition 
-	} elseif ( in_category( 'press' ) || post_is_in_descendant_category( 73 ) ) { // press: calling a function to check if a post is in a descendant category (so as not to list all subcats here. This won't work for notables, where the parent category is not the page 
-		$page_to_go_to = 'Press/Media'; // press 
+		$page_to_go_to = 'Miscellaneous/Appeals/Extradition'; // notables: miscellaneous-appeals-extradition
+	} elseif ( in_category( 'press' ) || post_is_in_descendant_category( 73 ) ) { // press: calling a function to check if a post is in a descendant category (so as not to list all subcats here. This won't work for notables, where the parent category is not the page
+		$page_to_go_to = 'Press/Media'; // press
 	} else {
-		$page_to_go_to = ''; // default calls page if page (good) or single post (dont really desire this but it's a fallback and a test for 2 conditions below) 
+		$page_to_go_to = ''; // default calls page if page (good) or single post (dont really desire this but it's a fallback and a test for 2 conditions below)
 	}
 
 	if ( ! is_attachment() ) {
@@ -69,7 +71,7 @@ endif; // end ! function_exists
 // 		elseif( get_field( 'download_pdf_file_url_1') && !get_field( 'download_pdf_file_url_2') ) {
 // 			$content .= the_field( 'link_language_1');
 // 		}
-// 	}	
+// 	}
 // 	return $content;
 // }
 // add_filter('the_content', 'pippin_filter_content_sample');
