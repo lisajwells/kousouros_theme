35bb18fdac363a8eeaf7fef6bb6c841092034c3b
lisajwells <lisa@lisajaynewells.com>
2018-04-18 12:13:29 -0400

    home scroll blurbs: adjust timing

---
+++ content-job-posting.php
@@ -0,0 +1,23 @@
+<!-- JOB Posting SECTION -->
+
+<div class="job-opening" style="width: 70%; max-width: 1170px; margin: 0 auto 3em;"><h3>JOB OPENING NOTICE</h3>
+<p><strong>POSITION:</strong> Legal Assistant</p>
+<p><strong>SALARY:</strong> $30.13 per hour</p>
+<p><strong>35 hours per week</strong></p>
+<p><strong>LOCATION:</strong> 260 Madison Avenue, 15th Floor</br>
+New York, NY 10016</p>
+<p><strong>JOB DUTIES:</strong> Conduct research to support legal proceedings and formulate defenses applying knowledge of American and U.K. jurisprudence including suppression, extraterritoriality and variance.</p>
+<p><strong>EDUCATION REQUIRED:</strong> Bachelor’s degree in Economics, Social Science or English</p>
+<p><strong>EXPERIENCE REQUIRED:</strong> 6 months in job offered or 6 months as Legal Intern</p>
+<p><strong>send resume:</strong> James Kousouros, 260 Madison Ave, 15th Fl, NY, NY 10016</p>
+
+<h3>JOB OPENING NOTICE</h3>
+<p><strong>POSITION:</strong> Legal Assistant</p>
+<p><strong>SALARY:</strong> $24.77 per hour</p>
+<p><strong>35 hours per week</strong></p>
+<p><strong>LOCATION:</strong> 260 Madison Avenue, 15th Floor</br>
+New York, NY 10016</p>
+<p><strong>JOB DUTIES:</strong> Conduct research to support legal proceedings and formulate defenses applying knowledge of American and U.K. jurisprudence including suppression, extraterritoriality and variance.</p>
+<p><strong>EDUCATION REQUIRED:</strong> Bachelor’s degree in Economics, Social Science or English</p>
+<p><strong>EXPERIENCE REQUIRED:</strong> 6 months in job offered or 6 months as Legal Intern</p>
+<p><strong>send resume:</strong> James Kousouros, 260 Madison Ave, 15th Fl, NY, NY 10016</p></div>
