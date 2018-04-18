457e35cb6bc9099384bcbdae319a07c95a6e9023
lisajwells <lisa@lisajaynewells.com>
2018-04-18 11:53:03 -0400

    add third animated intro blurb

--- page-home.php
+++ page-home.php
@@ -16,13 +16,16 @@ get_header(); ?>
             <img class="home-slider-img fade-in" id="home-slider-img-2" src="<?php echo content_url(); ?>/uploads/2015/06/HomePage_Center.jpg">
             <img class="home-slider-img fade-in" id="home-slider-img-3" src="<?php echo content_url(); ?>/uploads/2015/06/HomePage_Right.jpg">
         <aside class="home-blurb fadeInUpOutUp" id="home-blurb-1">
-            <p>His distinguished reputation and fierce advocacy command superior results.</p>
+            <p>Expert. Strategic. Criminal defense.</p>
         </aside>
         <aside class="home-blurb fadeInUpOutUp" id="home-blurb-2">
-            <p>Fighting vigorously, relentlessly and strategically at every stage of your defense.</p>
+            <p>Complex litigation for professionals and businesses.</p>
+        </aside>
+        <aside class="home-blurb fadeInUpOutUp" id="home-blurb-3">
+            <p>Excellent results.</p>
         </aside>
         </div>
-    </div>    
+    </div>
 
     <p id="specializing" style="text-align: center;"><strong>Specializing in state and federal criminal defense in New York and throughout the country</strong></p>
 </div>
