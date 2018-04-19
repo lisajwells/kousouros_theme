4b5bc54a60ea30456b3035b12bbb48ab7457c239
lisajwells <lisa@lisajaynewells.com>
2018-04-19 14:32:14 -0400

    add misc press section

--- page-press.php
+++ page-press.php
@@ -16,21 +16,21 @@ get_header(); ?>
 		<div id="primary" class="content-area-wide col-md-12">
 			<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-        <!-- add id of page id and class of .press_loop -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'press_loop' ); ?>>
+	        	<!-- add id of page id and class of .press_loop -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'press_loop' ); ?>
 
 					<div class="row">
 						<div class="col-md-12 press-subhed">
 							<h3 id="print-subhed">Print</h3>
 							<p class="intro-graph">These articles relate to cases Mr. Kousouros has handled or is currently handling, as well as articles for which he was sought out as an expert in criminal law.</p>
 						</div>
-					</div>
+					</div><!-- row -->
 
 					<div class="row"><!-- Here starts the 2 Press-Print columns -->
 						<div class="entry-content-press1 col-md-5 col-md-offset-1"><!-- -->
-							<?php 
+							<?php
 								$press_print1_query = new WP_Query( 'category_name=daily-news');
 								$press_print2_query = new WP_Query( 'category_name=new-york-times');
 								$press_print6_query = new WP_Query( 'category_name=reuters');
@@ -77,9 +77,10 @@ get_header(); ?>
 						</div><!-- .entry-content -->
 
 						<div class="entry-content-press2 col-md-5"><!-- -->
-							<?php 
+							<?php
 								$press_print3_query = new WP_Query( 'category_name=journal-news');
 								$press_print4_query = new WP_Query( 'category_name=newsday');
+								$press_print5_query = new WP_Query( 'category_name=miscellaneous-print');
 							?>
 
 							<figure class="press-logo">
@@ -100,64 +101,72 @@ get_header(); ?>
 							<?php endwhile; // end of the loop. ?>
             				<?php wp_reset_postdata(); ?>
 
-					</div><!-- .entry-content -->
-				</div><!-- .row Print -->
+							<figure class="press-logo border-above">
+							</figure>
+
+							<?php while ( $press_print5_query->have_posts() ) : $press_print5_query->the_post(); ?>
+								<?php get_template_part( 'content', 'press-print' ); ?>
+							<?php endwhile; // end of the loop. ?>
+            				<?php wp_reset_postdata(); ?>
+
+						</div><!-- .entry-content -->
+					</div><!-- .row Print -->
 
 
-				<div class="row"><!-- Here starts the TV section -->
-					<div class="col-md-12 press-subhed">
-						<h3 id="tv-subhed">Cable and Network Appearances </h3>
+					<div class="row"><!-- Here starts the TV section -->
+						<div class="col-md-12 press-subhed">
+							<h3 id="tv-subhed">Cable and Network Appearances </h3>
+						</div>
 					</div>
-				</div>
 
-				<div class="row">
-					<div class="entry-content-press3 col-md-5 col-md-offset-1">
+					<div class="row">
+						<div class="entry-content-press3 col-md-5 col-md-offset-1">
 
-						<?php 
-							$press_tv1_query = new WP_Query( 'category_name=tv&tag=tv-column-1');
-						?>
+							<?php
+								$press_tv1_query = new WP_Query( 'category_name=tv&tag=tv-column-1');
+							?>
 
-						<?php while ( $press_tv1_query->have_posts() ) : $press_tv1_query->the_post(); ?>
-								<?php get_template_part( 'content', 'press-tv' ); ?>
-						<?php endwhile; // end of the loop. ?>
-            			<?php wp_reset_postdata(); ?>
+							<?php while ( $press_tv1_query->have_posts() ) : $press_tv1_query->the_post(); ?>
+									<?php get_template_part( 'content', 'press-tv' ); ?>
+							<?php endwhile; // end of the loop. ?>
+	            			<?php wp_reset_postdata(); ?>
 
-					</div><!-- .entry-content-press3 -->
+						</div><!-- .entry-content-press3 -->
 
-					<div class="entry-content-press3 col-md-5">
-						
-						<?php 
-							$press_tv2_intro = new WP_Query( 'category_name=tv&tag=tv-column-2-intro');
-						?>
-						<?php while ( $press_tv2_intro->have_posts() ) : $press_tv2_intro->the_post(); ?>
+						<div class="entry-content-press3 col-md-5">
+
+							<?php
+								$press_tv2_intro = new WP_Query( 'category_name=tv&tag=tv-column-2-intro');
+							?>
+							<?php while ( $press_tv2_intro->have_posts() ) : $press_tv2_intro->the_post(); ?>
 							<div class="press-tv-post press-tv-text">
 								<p class="press-title"><?php  the_title();  ?></p>
 								<p class="press-content"><?php echo get_the_content(); ?></p>
 							</div>
-						<?php endwhile; // end of the loop. ?>
-            			<?php wp_reset_postdata(); ?>
+							<?php endwhile; // end of the loop. ?>
+	            			<?php wp_reset_postdata(); ?>
 
-						<?php 
-							$press_tv2_query = new WP_Query( 'category_name=tv&tag=tv-column-2');
-						?>
+							<?php
+								$press_tv2_query = new WP_Query( 'category_name=tv&tag=tv-column-2');
+							?>
 
-						<?php while ( $press_tv2_query->have_posts() ) : $press_tv2_query->the_post(); ?>
-								<?php get_template_part( 'content', 'press-tv' ); ?>
-						<?php endwhile; // end of the loop. ?>
-            			<?php wp_reset_postdata(); ?>
+							<?php while ( $press_tv2_query->have_posts() ) : $press_tv2_query->the_post(); ?>
+									<?php get_template_part( 'content', 'press-tv' ); ?>
+							<?php endwhile; // end of the loop. ?>
+	            			<?php wp_reset_postdata(); ?>
 
-            			<!-- link to youtube channel -->
-            			<div id="youtube-cta">
-							<a target="_blank" href="https://www.youtube.com/channel/UCf0p1mIGTRomb7qsA9648oQ"><i class="fa fa-youtube-play"></i></a>
-							<p class="press-title"><a href="https://www.youtube.com/channel/UCf0p1mIGTRomb7qsA9648oQ" target="_blank">Visit Mr. Kousouros’ YouTube channel for<br> additional news coverage</a></p>
-            			</div>
+	            			<!-- link to youtube channel -->
+	            			<div id="youtube-cta">
+								<a target="_blank" href="https://www.youtube.com/channel/UCf0p1mIGTRomb7qsA9648oQ"><i class="fa fa-youtube-play"></i></a>
+								<p class="press-title"><a href="https://www.youtube.com/channel/UCf0p1mIGTRomb7qsA9648oQ" target="_blank">Visit Mr. Kousouros’ YouTube channel for<br> additional news coverage</a></p>
+	            			</div>
 
-					</div><!-- .entry-content-press3 -->
-				</div><!-- .row TV -->
+						</div><!-- .entry-content-press3 -->
+					</div><!-- .row TV -->
 
-			</article><!-- #post-## -->
+				</article><!-- #post-## -->
 
-		<?php endwhile; // end of the big loop. ?>
+				<?php endwhile; // end of the big loop. ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
