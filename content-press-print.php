d6b6dfb407f39d8bee2492d8c7c36cd5602abb86
lisajwells <lisa@lisajaynewells.com>
2018-04-24 18:11:50 -0400

    press print: content template reflects new publication field

--- content-press-print.php
+++ content-press-print.php
@@ -1,10 +1,13 @@
 <?php if( get_field( 'external_link') ): ?>
-	<h4 class="press-title press-print"><a target="_blank"  class="press-pdf" href="<?php the_field('external_link'); ?>"><?php  the_title();  ?></a></h4>
+	<h4 class="press-title press-print"><a target="_blank" class="press-pdf" href="<?php the_field('external_link'); ?>"><?php the_title(); ?></a></h4>
 <?php else: ?>
-	<h4 class="press-title press-print"><?php  the_title();  ?></h4>
+	<h4 class="press-title press-print"><?php the_title(); ?></h4>
+<?php endif; ?>
+
+<?php if( get_field( 'publication') ): ?>
+    <p class="press-publication"><?php the_field('publication'); ?></p>
 <?php endif; ?>
 
 <?php if( get_field( 'download_pdf_file_url') ): ?>
-	<a target="_blank"  class="press-pdf press-print" href="<?php the_field('download_pdf_file_url'); ?>">Download PDF</a>
-<?php else: ?>
+    <a target="_blank"  class="press-pdf press-print" href="<?php the_field('download_pdf_file_url'); ?>">Download PDF</a>
 <?php endif; ?>
