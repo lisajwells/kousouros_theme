42dbf7ca49fafdda63ac0a5053f79bf6e2b5c345
lisajwells <lisa@lisajaynewells.com>
2018-04-24 15:57:09 -0400

    home notables and profile: change colors

--- content-home-notables.php
+++ content-home-notables.php
@@ -1,21 +1,21 @@
 <!-- /* Most of this content is generated through Custom Fields added through Home Page edit. */ -->
 
 <!-- HOME NOTABLES SECTION -->
-<div class="section bg-darkgray" id="home-notable"><!-- home notable -->
+<div class="section" id="home-notable"><!-- home notable -->
     <div class="container">
         <div class="row">
 
             <div class="col-md-5 col-md-offset-1" id="home-notables-1">
-                <h2><?php the_field('home_notables_title_1'); ?></h2>    
+                <h2><?php the_field('home_notables_title_1'); ?></h2>
                 <p><?php the_field('home_notables_body_1'); ?></p>
             </div><!-- col-6 -->
 
             <div class="col-md-5" id="home-notables-2">
-                <h2><?php the_field('home_notables_title_2'); ?></h2>    
+                <h2><?php the_field('home_notables_title_2'); ?></h2>
                 <p><?php the_field('home_notables_body_2'); ?></p>
             </div><!-- col-6 -->
 
         </div><!-- row -->
-    <p class="center"><a class="btn btn-hollow" href="<?php bloginfo('url'); ?>/notable-cases/white-collar">Read notable cases</a></p>
+    <p class="center"><a class="btn btn-hollow-blackborder" href="<?php bloginfo('url'); ?>/notable-cases/white-collar">Read notable cases</a></p>
     </div><!-- container-->
 </div><!-- section home-notable-->
