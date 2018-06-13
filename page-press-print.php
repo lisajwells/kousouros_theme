2caec0469c2356761eefa50fd91d935ecfd21c84
lisajwells <lisa@lisajaynewells.com>
2018-06-13 19:25:54 -0400

    repair div nesting, adding .entry-content

--- page-press-print.php
+++ page-press-print.php
@@ -11,36 +11,37 @@ get_header(); ?>
 
 <div class="container">
 	<div id="main-grid" class="row">
-		<div id="primary" class="content-area-wide col-md-12">
+
+		<div id="primary" class="content-area col-md-12">
 			<main id="main" class="site-main" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
 	        	<!-- add id of page id and class of .press_loop -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'press_loop' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'press_loop' ); ?>>
+					<div class="entry-content">
+						<div class="row">
+							<div class="col-md-12 press-subhed">
+								<p class="intro-graph">These articles relate to cases Mr. Kousouros has handled or is currently handling, as well as articles for which he was sought out as an expert in criminal law.</p>
+							</div>
+						</div><!-- row -->
 
-					<div class="row">
-						<div class="col-md-12 press-subhed">
-							<p class="intro-graph">These articles relate to cases Mr. Kousouros has handled or is currently handling, as well as articles for which he was sought out as an expert in criminal law.</p>
-						</div>
-					</div><!-- row -->
+						<div class="row"><!-- Here starts the Press-Print column -->
+							<div class="entry-content-press1"><!-- -->
+								<?php
+									$press_print1_query = new WP_Query( 'category_name=print');
 
-					<div class="row"><!-- Here starts the Press-Print column -->
-						<div class="entry-content-press1"><!-- -->
-							<?php
-								$press_print1_query = new WP_Query( 'category_name=print');
+								?>
+								<?php while ( $press_print1_query->have_posts() ) : $press_print1_query->the_post(); ?>
+									<!-- get content-press-print.php template part -->
+									<?php get_template_part( 'content', 'press-print' ); ?>
+								<?php endwhile; // end of the loop. ?>
+	            				<?php wp_reset_postdata(); ?>
 
-							?>
-							<?php while ( $press_print1_query->have_posts() ) : $press_print1_query->the_post(); ?>
-								<!-- get content-press-print.php template part -->
-								<?php get_template_part( 'content', 'press-print' ); ?>
-							<?php endwhile; // end of the loop. ?>
-            				<?php wp_reset_postdata(); ?>
+							</div><!-- .entry-content-press1 -->
 
-						</div><!-- .entry-content -->
-
-					</div><!-- .row Print -->
-
+						</div><!-- .row Print -->
+					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
 
 				<?php endwhile; // end of the big loop. ?>
