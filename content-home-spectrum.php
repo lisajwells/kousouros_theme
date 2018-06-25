2015a3bf418981b40087e95417f2901cfaafa301
Lisa Wells <lisawells999@gmail.com>
2018-06-25 13:41:59 -0400

    home spectrum: add link to header

--- content-home-spectrum.php
+++ content-home-spectrum.php
@@ -6,21 +6,25 @@
         <div class="row" id="home-spectrum-row">
 
             <div class="col-md-3 home-spectrum">
+                <h3><a href="<?php the_field('home_spectrum_link_1'); ?>"><?php the_field('home_spectrum_heading_1'); ?></a></h3>
                 <?php the_field('home_spectrum_text_1'); ?>
                 <a class="home-cta-a home-cta-a-white" href="<?php the_field('home_spectrum_link_1'); ?>">Firm Overview</a>
             </div>
 
             <div class="col-md-3 home-spectrum">
+                <h3><a href="<?php the_field('home_spectrum_link_2'); ?>"><?php the_field('home_spectrum_heading_2'); ?></a></h3>
                 <?php the_field('home_spectrum_text_2'); ?>
                 <a class="home-cta-a home-cta-a-white" href="<?php the_field('home_spectrum_link_2'); ?>">Services Provided</a>
             </div>
 
             <div class="col-md-3 home-spectrum">
+                <h3><a href="<?php the_field('home_spectrum_link_3'); ?>"><?php the_field('home_spectrum_heading_3'); ?></a></h3>
                 <?php the_field('home_spectrum_text_3'); ?>
                 <a class="home-cta-a home-cta-a-white" href="<?php the_field('home_spectrum_link_3'); ?>">Profile</a>
             </div>
 
             <div class="col-md-3 home-spectrum">
+                <h3><a href="<?php the_field('home_spectrum_link_4'); ?>"><?php the_field('home_spectrum_heading_4'); ?></a></h3>
                 <?php the_field('home_spectrum_text_4'); ?>
                 <a class="home-cta-a home-cta-a-white" href="<?php the_field('home_spectrum_link_4'); ?>">The Process</a>
             </div>
