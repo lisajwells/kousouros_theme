ed4c9a468ba030ae838bdce31a4fdb15a1b01999
Lisa Wells <lisawells999@gmail.com>
2018-06-25 11:58:01 -0400

    home hero: add options for text below

--- page-home.php
+++ page-home.php
@@ -27,12 +27,36 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="container"><p id="specializing">The Law Offices of James Kousouros is a criminal defense firm with
-            extensive experience in all areas of criminal law, including complex criminal litigation. James Kousouros,
-            the founder and principal of the firm, has over 30 years experience fighting, and winning, life changing
-            battles in state and federal courtrooms in New York and throughout the country. He and his team have
-            represented a wide range of clients, including doctors, pharmacists, CEO’s, designers, members of the
-            financial community, lawyers and business owners.</p></div>
+    <div class="container">
+        <p class="specializing"><strong>The Law Offices of James Kousouros</strong> specializes in complex litigation, and all other areas of criminal law.</p>
+        <p class="specializing"><strong>For over 30 years</strong> James Kousouros, the founder and principal of the firm, has been fighting and winning life changing battles in state and federal criminal courtrooms in New York and throughout the country.</p>
+        <p class="specializing"><strong>He and his team</strong> have represented a wide range of clients, including doctors, pharmacists, CEO’s, designers, members of the financial community, lawyers and business owners.</p>
+    </div>
+
+</div>
+<div class="section" id="home-slider">
+    <div class="container" id="home-slider-container">
+        <div class="row home-images">
+            <img class="home-slider-img fade-in" id="home-slider-img-1"
+                 src="<?php echo content_url(); ?>/uploads/2018/06/Courts.jpg">
+
+            <aside class="home-blurb fadeInUpOutUp" id="home-blurb-1">
+                <p>Expert. Strategic. Criminal defense.</p>
+            </aside>
+            <aside class="home-blurb fadeInUpOutUp" id="home-blurb-2">
+                <p>Complex litigation for professionals and businesses.</p>
+            </aside>
+            <aside class="home-blurb fadeInUpOutUp" id="home-blurb-3">
+                <p>Distinguished Reputation. Excellent results.</p>
+            </aside>
+        </div>
+    </div>
+
+    <div class="container"><p class="specializing2"><strong>The Law Offices of James Kousouros provides:</strong></p>
+        <ul><li>A full service team that specializes in complex litigation and all other areas of criminal law</li>
+            <li>Over 30 years experience fighting and winning life changing battles in state and federal courts</li>
+            <li>Representation for a wide range of clients, including doctors, pharmacists, CEO’s, designers, members of the financial community, lawyers and business owners</li></ul>
+    </div>
 
 </div>
 
